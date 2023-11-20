39b3054bed637fd0779b43cb064262ae0a7e4c22
priya17910 <priyagupta17910@gmail.com>
2023-11-20 20:23:33 +0530

    Update Main.java bst to skew tree

--- src/com/greatlearning/bstskewtree/driver/Main.java
+++ src/com/greatlearning/bstskewtree/driver/Main.java
@@ -24,38 +24,45 @@ public class Main {
 		{
 			System.out.println ("Invalid Input. Please enter an integer.");
 		}
-		System.out.println ("Enter the " + numOfValues + " number of keys : ");
-		// TRY CATCH BLOCK TO HANDLE INPUT MISMATCH EXCEPTION
-		try
+		if (numOfValues > 0)
 		{
-			val = sc.nextInt();
-			// CREATING ROOT OF THE BINARY SEARCH TREE
-			root = new TreeNode (val);
-		}
-		catch (java.util.InputMismatchException e)
-		{
-			System.out.println ("Invalid Input. Please enter an integer.");
-		}
-		for (int i = 1; i < numOfValues; i++)
-		{
+			System.out.println ("Enter the " + numOfValues + " number of keys : ");
 			// TRY CATCH BLOCK TO HANDLE INPUT MISMATCH EXCEPTION
 			try
 			{
 				val = sc.nextInt();
-				root.insert(root, val);
+				// CREATING ROOT OF THE BINARY SEARCH TREE
+				root = new TreeNode (val);
 			}
 			catch (java.util.InputMismatchException e)
 			{
 				System.out.println ("Invalid Input. Please enter an integer.");
 			}
+			for (int i = 1; i < numOfValues; i++)
+			{
+				// TRY CATCH BLOCK TO HANDLE INPUT MISMATCH EXCEPTION
+				try
+				{
+					val = sc.nextInt();
+					root.insert(root, val);
+				}
+				catch (java.util.InputMismatchException e)
+				{
+					System.out.println ("Invalid Input. Please enter an integer.");
+				}
+			}
+			/* TAKING INPUT FROM USER ENDS HERE */
+			
+			/* CALLING ACTUAL METHOD TO FLATTEN THE BST INTO A RIGHT SKEWED TREE */
+			root = FlattenAndPrintBST.flattenBST(root);
+			
+			/* CALLING METHOD TO PRINT TRAVERSAL OF RIGHT SKEWED TREE */
+			FlattenAndPrintBST.printSkewTreeTraversal(root);
 		}
-		/* TAKING INPUT FROM USER ENDS HERE */
-		
-		/* CALLING ACTUAL METHOD TO FLATTEN THE BST INTO A RIGHT SKEWED TREE */
-		root = FlattenAndPrintBST.flattenBST(root);
-		
-		/* CALLING METHOD TO PRINT TRAVERSAL OF RIGHT SKEWED TREE */
-		FlattenAndPrintBST.printSkewTreeTraversal(root);
+		else
+		{
+			System.exit(0);
+		}
 		sc.close();
 	}
 }
